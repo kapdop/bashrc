38371682f7a30499f3cc42dbb07e335d246d5a96
chasmodo <bosko.trkulja@gmail.com>
2016-06-18 14:10:40 +0200

    Add cloning shit

---
+++ clone.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd
+
+git clone https://github.com/w3nd1go/Themes
+git clone https://github.com/w3nd1go/Sardi
+git clone https://github.com/w3nd1go/Wallpapers
+git clone https://github.com/kapdop/bashrc
+git clone https://github.com/kapdop/Mint_Customize
+
+exit
